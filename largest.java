e9348044e232341a233416e710adccc9c2c24e52
Sujayde <47351835+Sujayde@users.noreply.github.com>
2019-02-06 14:26:47 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a,b,c;
+	Scanner s=new Scanner(System.in);
+	a=s.nextInt();
+	b=s.nextInt();
+	c=s.nextInt();
+
+	if(a>b&&a>c)
+	
+	System.out.println(a);
+	
+	else if(b>a&&b>c)
+	System.out.println(b);
+	
+	else if(c>a&&c>b)
+	
+	System.out.println(c);
+	
+	else
+	
+	System.out.println("notdistring");
+	
+}
+}
